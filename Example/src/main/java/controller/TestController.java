80bf006483c5f80b44594873b71c7b5294cf5f30
hkpp6548 <510373198@qq.com>
2017-08-28 23:36:03 +0800

    增加一些目录结构
    增加一些目录结构

---
+++ Example/src/main/java/controller/TestController.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * Created by hk on 2017/8/28.
+ */
+public class TestController {
+    //测试控制器
+}
---
+++ Example/src/test/java/controller/ToTestController.java
@@ -0,0 +1,8 @@
+package controller;
+
+/**
+ * Created by hk on 2017/8/28.
+ */
+public class ToTestController {
+
+}
